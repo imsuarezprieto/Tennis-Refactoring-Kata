681dc984c79bcf9f266f865e13b2092830a0b157
issuarez <issuarez@aidacanarias.com>
2021-11-05 14:23:30 +0100

    🎨 Apply code restyle

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,34 +1,42 @@
 namespace Tennis
 {
-    class TennisGame1 : ITennisGame
-    {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+    using System;
 
-        public TennisGame1(string player1Name, string player2Name)
+
+    internal class TennisGame1 : ITennisGame
+    {
+
+        private Int32 m_score1  ;
+        private Int32 m_score2  ;
+        private String player1Name;
+        private String player2Name;
+
+
+        public TennisGame1 (String player1Name, String player2Name)
         {
             this.player1Name = player1Name;
             this.player2Name = player2Name;
         }
 
-        public void WonPoint(string playerName)
+
+        public void WonPoint (String playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
-            else
-                m_score2 += 1;
+            if (playerName == "player1") {
+                this.m_score1 += 1;
+            }
+            else {
+                this.m_score2 += 1;
+            }
         }
 
-        public string GetScore()
+
+        public String GetScore ()
         {
-            string score = "";
+            var score     = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
-            {
-                switch (m_score1)
-                {
+
+            if (this.m_score1 == this.m_score2) {
+                switch (this.m_score1) {
                     case 0:
                         score = "Love-All";
                         break;
@@ -41,25 +49,35 @@ namespace Tennis
                     default:
                         score = "Deuce";
                         break;
-
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+            else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+                Int32 minusResult = this.m_score1 - this.m_score2;
+
+                if (minusResult == 1) {
+                    score = "Advantage player1";
+                }
+                else if (minusResult == -1) {
+                    score = "Advantage player2";
+                }
+                else if (minusResult >= 2) {
+                    score = "Win for player1";
+                }
+                else {
+                    score = "Win for player2";
+                }
             }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
+            else {
+                for (var i = 1; i < 3; i++) {
+                    if (i == 1) {
+                        tempScore = this.m_score1;
+                    }
+                    else {
+                        score += "-";
+                        tempScore = this.m_score2;
+                    }
+
+                    switch (tempScore) {
                         case 0:
                             score += "Love";
                             break;
@@ -75,8 +93,9 @@ namespace Tennis
                     }
                 }
             }
+
             return score;
         }
+
     }
 }
-
